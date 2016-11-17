10b0dba829e947af3d9e6eb31d6d0e50e84996fc
MathanKA <amheretoinspire@hotmail.com>
2016-11-17 15:27:46 +0530

    item method added

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { TodoService } from './todostore.service';
 
 @Component({
   selector: 'todo',
-  templateUrl: 'app/todo.html'
+  templateUrl: 'app/todo.html',
+  providers: [TodoService]
 })
 
 
-export class AppComponent { }
+export class AppComponent {
+  newtodo: any;
+  todolists;
+
+  // constructor(){
+  //   this.newtodo = '';
+  //   this.todolists = [];
+  // }
+  constructor(todoService: TodoService){
+    this.todolists = todoService;
+  }
+  
+  // addTodo() {
+  //   this.todolists.push(this.newtodo);
+  //   this.newtodo = '';
+  //   console.log(this.todolists);
+  // }
+  addTodo(){
+		if (this.newtodo.trim().length) {
+			this.todolists.add(this.newtodo);
+			this.newtodo = '';
+		}
+    console.log(this.todolists);
+  }
+ }
---
+++ app/todostore.service.ts
@@ -0,0 +1,10 @@
+export class TodoService {
+    todoItem:String ;
+    todos = [];
+
+addItem(todoItem){
+    this.todos.push(this.todoItem);
+}
+
+
+}
